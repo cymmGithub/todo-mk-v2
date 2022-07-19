f082ce6baac88ddd461279e1987a54774e5b5233
cymmgithub <przemswiercz@gmail.com>
2022-07-19 16:02:34 +0200

    changed port

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use('/todo', todoRouter);
 
-app.listen(3000, () => {
-  console.log('Listening on 3000');
-});
+app.listen(
+  process.env.PORT || 3000,
+  () => console.log('Server is running...'),
+);
